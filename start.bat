4e0b58c5df136cbe609bc2797235cab714d9ed16
valijon571 <148867160+valijon571@users.noreply.github.com>
2024-06-07 22:52:05 +0500

    update file

---
+++ start.bat
@@ -0,0 +1,23 @@
+@echo off
+cd /d %~dp0
+
+title Checking Python installation...
+python --version > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed! (Go to https://www.python.org/downloads and install the latest version.^)
+    echo Make sure it is added to PATH
+    goto ERROR
+)
+
+title Checking libraries...
+pip install requests pymem ctypes
+cls
+title Starting hyperinjector...
+python hyperinjector.py
+if %errorlevel% neq 0 goto ERROR
+exit
+
+:ERROR
+color 4 && title [Error]
+echo ERROR contact not.darian on Discord
+pause > nul
